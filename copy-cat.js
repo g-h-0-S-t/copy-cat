7da2f49152bf0005813842808a4472aefabd1b3a
gh0$t <6196046+g-h-0-S-t@users.noreply.github.com>
2022-01-29 06:32:48 +0530

    Update copy-cat.js

--- copy-cat.js
+++ copy-cat.js
@@ -24,7 +24,7 @@ javascript:
 
     (function(window) {
         var tab = window.open('about:blank', '_blank');
-        tab.document.write('<h1 id="copy-cat-holder"></h1><br>');
+        tab.document.write('<h1 id="copy-cat-holder"></h1>');
         tab.document.write(document.body.outerHTML);
         var downloadLink = 'data:text/html;base64,' + btoa(unescape(encodeURIComponent(tab.document.documentElement.innerHTML)));
         tab.document.getElementById('copy-cat-holder').innerHTML = '<a id="copy-cat" download="Download.html" style="position: fixed !important; top: 0 !important; left: 0 !important; width: 100% !important; margin: 0 auto !important; background: #0a0a0a !important; color: #c3015c !important; z-index: 999999999999999999999999999999 !important; height: 100px !important; font-size: 50px !important; text-align: center !important; line-height: 100px !important; font-family: "Gill Sans", sans-serif !important;">Download as HTML</a>';
